e5b472b2bc2f9305eb59d4a00169020558872bff
Benny <chatgptanta@gmail.com>
2026-07-06 14:54:38 +0800

    测试提交0706001

--- src/main/java/com/example/pipelinedemo0702/controller/HelloController.java
+++ src/main/java/com/example/pipelinedemo0702/controller/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
     // 访问地址：http://localhost:8080/hello
     @GetMapping("/hello")
     public String hello() {
-        return "**********HELLO FROM BENNY!!! ********";
+        return "**********HELLO FROM Freedotech!!! ********";
     }
 }
